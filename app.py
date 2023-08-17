52d44b0c1598c77a5993415e2031f427ee9be2d7
Rodrigo Barbosa <rodrigodot42@gmail.com>
2023-08-17 01:38:33 +0000

    pipe test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Oi, vc que está aí do outro lado"
+    return "Oi, vc que está aí do outro lado, v1"
 
 if __name__ == '__main__':
     app.run()
