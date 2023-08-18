12034f35070914270914e10ef0755aa1de8ae0a3
Rodrigo Barbosa <rodrigodot42@gmail.com>
2023-08-17 23:36:45 -0300

    print test

--- test.py
+++ test.py
@@ -1,50 +1,56 @@
 # -*- coding: utf-8 -*-
-import pytest
-from selenium.webdriver.common.by import By
-import sys
+# import pytest
+# from selenium.webdriver.common.by import By
+# import sys
 
-@pytest.mark.usefixtures('driver')
-class TestLink:
-    # def setUp(self):
-    #     # cria uma instância do unittest, precisa do nome "setUp"
-    #     self.app = app.test_client()
+from app import app
+import unittest
 
-    #     # envia uma requisicao GET para a URL
-    #     self.result = self.app.get('/')
 
-    # def test_requisicao(self):
-    #     # compara o status da requisicao (precisa ser igual a 200)
-    #     self.assertEqual(self.result.status_code, 200)
 
-    # def test_conteudo(self):
+# @pytest.mark.usefixtures('driver')
+class Test(unittest.TestCase):
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
+
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
+
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
+
+    def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        # self.assertEqual(self.result.data.decode('utf-8'), "Rodrigo")
+        print('PRINT >>>>>>>>>>', self.result.data.decode('utf-8'))
+        self.assertEqual(self.result.data.decode('utf-8'), "Rodrigo")
 
 
-    def test_content(self):
-        driver_node = webdriver.Chrome(ChromeDriverManager().install())
-        self.driver=driver_node
-        self.driver.maximize_window()
+    # def test_content(self):
+    #     driver_node = webdriver.Chrome(ChromeDriverManager().install())
+    #     self.driver=driver_node
+    #     self.driver.maximize_window()
 
-        self.driver.get('/')
-        self.driver.implicitly_wait(10)
+    #     self.driver.get('/')
+    #     self.driver.implicitly_wait(10)
 
-        title1_text = "Rodrigo"
-        text_title1 = driver.find_element(By.ID, "title1")
-        text_title1.send_keys(title1_text)
+    #     title1_text = "Rodrigo"
+    #     text_title1 = driver.find_element(By.ID, "title1")
+    #     text_title1.send_keys(title1_text)
 
-        assert text_title1 == title1_text
+    #     assert text_title1 == title1_text
 
-        title2_text = "Barbosa"
-        text_title2 = driver.find_element(By.ID, "title2")
-        text_title2.send_keys(title2_text)
+    #     title2_text = "Barbosa"
+    #     text_title2 = driver.find_element(By.ID, "title2")
+    #     text_title2.send_keys(title2_text)
 
-        assert text_title2 == title2_text
+    #     assert text_title2 == title2_text
 
-        title3_text = "Costa"
-        text_field3 = driver.find_element(By.ID, "title3")
-        text_field3.send_keys(title3_text)
+    #     title3_text = "Costa"
+    #     text_field3 = driver.find_element(By.ID, "title3")
+    #     text_field3.send_keys(title3_text)
 
-        assert text_field3 == title3_text
+    #     assert text_field3 == title3_text
 
 # https://www.lambdatest.com/learning-hub/pytest-tutorial
