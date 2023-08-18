e817ef18bf7a59f0e5ac1fc0a6d2f97b5614d712
Rodrigo Barbosa <rodrigodot42@gmail.com>
2023-08-17 22:27:01 -0300

    update title

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
 from app import app
 import unittest
 
+from selenium.webdriver.common.by import By
+import sys
+
 class Test(unittest.TestCase):
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
@@ -17,3 +20,13 @@ class Test(unittest.TestCase):
     # def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
         # self.assertEqual(self.result.data.decode('utf-8'), "Rodrigo")
+
+
+    # def test_item(self, driver):
+    #    driver.get('/')
+    #    sample_text = "Rodrigo"
+    #    text_field = driver.find_element(By.ID, "sampletodotext")
+    #    email_text_field.send_keys(sample_text)
+
+
+    #    driver.find_element(By.ID, "addbutton").click()
