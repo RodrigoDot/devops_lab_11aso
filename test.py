1b9f983deb93130ed4804a8635c4d9ccdcd102f8
Rodrigo Barbosa <rodrigodot42@gmail.com>
2023-08-17 22:39:07 -0300

    update test file

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ from selenium.webdriver.common.by import By
 import sys
 
 @pytest.mark.usefixtures('driver')
-class Test(unittest.TestCase):
-    def setUp(self):
-        # cria uma instância do unittest, precisa do nome "setUp"
-        self.app = app.test_client()
+class Test:
+    # def setUp(self):
+    #     # cria uma instância do unittest, precisa do nome "setUp"
+    #     self.app = app.test_client()
 
-        # envia uma requisicao GET para a URL
-        self.result = self.app.get('/')
+    #     # envia uma requisicao GET para a URL
+    #     self.result = self.app.get('/')
 
-    def test_requisicao(self):
-        # compara o status da requisicao (precisa ser igual a 200)
-        self.assertEqual(self.result.status_code, 200)
+    # def test_requisicao(self):
+    #     # compara o status da requisicao (precisa ser igual a 200)
+    #     self.assertEqual(self.result.status_code, 200)
 
     # def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
